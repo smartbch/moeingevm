dfc9897f82d5ad9768664b747ad1d8dc48417f6e
zxh0 <zhangxiuhong2013@gmail.com>
2022-01-22 21:47:00 +0800

    Refine ChainEvent

--- types/header.go
+++ types/header.go
@@ -2,14 +2,24 @@ package types
 
 import (
 	gethcmn "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	gethcore "github.com/ethereum/go-ethereum/core/types"
 )
 
 type Header struct {
-	Number    uint64
-	BlockHash gethcmn.Hash
-	Bloom     gethcore.Bloom
-	// TODO: add more fields
+	Number     hexutil.Uint64  `json:"number"`
+	BlockHash  gethcmn.Hash    `json:"hash"`
+	ParentHash gethcmn.Hash    `json:"parentHash"`
+	Bloom      gethcore.Bloom  `json:"logsBloom"`
+	TxRoot     gethcmn.Hash    `json:"transactionsRoot"`
+	StateRoot  gethcmn.Hash    `json:"stateRoot"`
+	Miner      gethcmn.Address `json:"miner"`
+	GasUsed    hexutil.Uint64  `json:"gasUsed"`
+	Timestamp  hexutil.Uint64  `json:"timestamp"`
+	//UncleHash   gethcmn.Hash   `json:"sha3Uncles"`
+	//ReceiptHash gethcmn.Hash   `json:"receiptsRoot"`
+	//Difficulty  *hexutil.Big   `json:"difficulty"`
+	//GasLimit    hexutil.Uint64 `json:"gasLimit"`
 }
 
 func (h *Header) Hash() gethcmn.Hash {
